360eaf799a2517ffb7b3d5da75f4ac1bfc2256ad
Felix Augustsson <git.fexilus@gmail.com>
2021-03-19 16:11:18 +0100

    Modularized prolongation

--- src/symmetries/generator.py
+++ src/symmetries/generator.py
@@ -8,7 +8,7 @@ from .utils import iter_wrapper
 
 
 class Generator:
-    """Create an operator on the form of an infinitesimal generator."""
+    """A local coordinate representation of an infinitesimal generator."""
 
     xis = []
     etas = []
@@ -20,31 +20,11 @@ class Generator:
         self.etas = [sympify(eta) for eta in iter_wrapper(etas)]
 
     def __call__(self, expr, jet_space):
-        eta_prolongations = {}
-        base_size = len(jet_space.base_space)
+        """Apply the generator on an expression on a jet space."""
 
-        for dependent, eta in zip(jet_space.fibres, self.etas):
-            eta_prolongations[dependent] = {(0,) * base_size: eta}
+        eta_prolongations = get_prolongations(self.xis, self.etas, jet_space)
 
-            multiindex_iter = iter(jet_space.fibres[dependent])
-            next(multiindex_iter)
-
-            for multiindex in multiindex_iter:
-                index_class = next((i for i, x in enumerate(multiindex) if x), None)
-                leading_deriv_index = (0,) * index_class + (1,) + (0,) * (base_size - index_class - 1)
-                leading_deriv_symbol = jet_space.base_space[index_class]
-
-                prev_index = tuple(map(operator.sub, multiindex, leading_deriv_index))
-                prev_prolongation = eta_prolongations[dependent][prev_index]
-
-                eta_prolongations[dependent][multiindex] = total_derivative(prev_prolongation, leading_deriv_symbol, jet_space)
-
-                for base_coord, xi in zip(jet_space.base_space, self.xis):
-                    base_index = jet_space.base_index(base_coord)
-                    derivative_index = tuple(map(operator.add, prev_index, base_index))
-
-                    eta_prolongations[dependent][multiindex] -= jet_space.fibres[dependent][derivative_index] * total_derivative(xi, base_coord, jet_space)
-
+        # Apply differential operation in each jet space coordinate
         out_expr = 0
 
         for base_coord, xi in zip(jet_space.base_space, self.xis):
@@ -52,7 +32,10 @@ class Generator:
 
         for dependent in jet_space.fibres:
             for multiindex in jet_space.fibres[dependent]:
-                out_expr += eta_prolongations[dependent][multiindex] * expr.diff(jet_space.fibres[dependent][multiindex])
+                derivative = expr.diff(jet_space.fibres[dependent][multiindex])
+                eta_prolongation = eta_prolongations[dependent][multiindex]
+
+                out_expr += eta_prolongation * derivative
 
         return out_expr
 
@@ -64,3 +47,58 @@ class Generator:
         eta_str = "  " + "\n  ".join(str(eta) for eta in self.etas)
 
         return f"Generator with xis:\n{xi_str}\nand etas:\n{eta_str}"
+
+
+def get_prolongations(xis, etas, jet_space):
+    """Calculate the coefficients of a vector field prolonged over a jet space.
+
+    The vector field is characterized by the coefficients of derivatives in
+    the base space (xis) and the coefficients of derivatives in the fiber of
+    the original fiber bundle from which the jet space is created.
+    """
+
+    eta_prolongations = {}
+    base_size = len(jet_space.base_space)
+
+    for dependent, eta in zip(jet_space.fibres, etas):
+
+        multiindex_iter = iter(jet_space.fibres[dependent])
+
+        eta_prolongations[dependent] = {(0,) * base_size: eta}
+        next(multiindex_iter)
+
+        for multiindex in multiindex_iter:
+            # Calculate the index class, ie. the "first" derivative's number
+            index_class = next(i for i, x in enumerate(multiindex) if x)
+
+            leading_deriv_index = ((0,) * index_class +
+                                   (1,) +
+                                   (0,) * (base_size - index_class - 1))
+            leading_deriv_symbol = jet_space.base_space[index_class]
+
+            # Calculate a lower order deivative from which the current
+            # derivative can be taken.
+            prev_index = tuple(map(operator.sub, multiindex,
+                                   leading_deriv_index))
+
+            prev_prolongation = eta_prolongations[dependent][prev_index]
+
+            # The D(eta_(n-1)) component of the prolongation formula
+            eta_component = total_derivative(prev_prolongation,
+                                             leading_deriv_symbol,
+                                             jet_space)
+            eta_prolongations[dependent][multiindex] = eta_component
+
+            # The omega_(n-1)*D(xi) components of the prolongation formula
+            for base_coord, xi in zip(jet_space.base_space, xis):
+                base_index = jet_space.base_index(base_coord)
+                derivative_index = tuple(map(operator.add, prev_index,
+                                             base_index))
+
+                deriv_coord = jet_space.fibres[dependent][derivative_index]
+                xi_term = deriv_coord * total_derivative(xi, base_coord,
+                                                         jet_space)
+
+                eta_prolongations[dependent][multiindex] -=  xi_term
+
+    return eta_prolongations
