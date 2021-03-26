cff013f3b4170c918fa178df495b4108abc8e757
Felix Augustsson <git.fexilus@gmail.com>
2021-03-26 15:03:02 +0100

    Added standard operators for generators

--- src/symmetries/generator.py
+++ src/symmetries/generator.py
@@ -71,13 +71,95 @@ class Generator:
 
         return False
 
+    def __add__(self, other):
+
+        if not isinstance(other, Generator):
+            return NotImplemented
+
+        if self.total_space != other.total_space:
+            raise NotImplementedError("Generators have to be in same coordinates")
+
+        sum_xis = []
+        for xi1, xi2 in zip(self.xis, other.xis):
+            sum_xis += [(xi1 + xi2).expand()]
+
+        sum_etas = []
+        for eta1, eta2 in zip(self.etas, other.etas):
+            sum_etas += [(eta1 + eta2).expand()]
+
+        return Generator(sum_xis, sum_etas, self.total_space)
+
+    def __sub__(self, other):
+
+        if not isinstance(other, Generator):
+            return NotImplemented
+
+        if self.total_space != other.total_space:
+            raise NotImplementedError("Generators have to be in same coordinates")
+
+        sum_xis = []
+        for xi1, xi2 in zip(self.xis, other.xis):
+            sum_xis += [(xi1 - xi2).expand()]
+
+        sum_etas = []
+        for eta1, eta2 in zip(self.etas, other.etas):
+            sum_etas += [(eta1 - eta2).expand()]
+
+        return Generator(sum_xis, sum_etas, self.total_space)
+    
+    def __truediv__(self, other):
+
+        quot_xis = []
+        for xi in self.xis:
+            quot_xis += [(xi / other).expand()]
+
+        quot_etas = []
+        for eta in self.etas:
+            quot_etas += [(eta / other).expand()]
+
+        return Generator(quot_xis, quot_etas, self.total_space)
+
+    def __rmul__(self, other):
+        # Only right multiplication is implemented, as left multiplication of
+        # the differential operator might be interpreted as application.
+
+        prod_xis = []
+        for xi in self.xis:
+            prod_xis += [(other * xi).expand()]
+
+        prod_etas = []
+        for eta in self.etas:
+            prod_etas += [(other * eta).expand()]
+
+        return Generator(prod_xis, prod_etas, self.total_space)
+    
+    def __neg__(self):
+
+        neg_xis = [(-xi).expand() for xi in self.xis]
+
+        neg_etas = [(-eta).expand() for eta in self.etas]
+
+        return Generator(neg_xis, neg_etas, self.total_space)
+
+    def __pos__(self):
+
+        pos_xis = [(+xi).expand() for xi in self.xis]
+
+        pos_etas = [(+eta).expand() for eta in self.etas]
+
+        return Generator(pos_xis, pos_etas, self.total_space)
+
 
 def generator_on(total_space):
-    """Returns a initiallization method for generators on the total space."""
+    """Returns a initiallization method for generators on the total space.
+
+    Is meant to be used in code where several generators on the same space are
+    used to reduce visual clutter.
+    """
 
     class _Generator(Generator):
         def __init__(self, xis, etas):
-            super(_Generator, self).__init__(xis, etas, total_space)
+            super().__init__(xis, etas, total_space)
 
     return _Generator
 
