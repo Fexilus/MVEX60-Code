9e5568a5fca8c4f56b321e47e4c5703f4f9fa5df
Felix Augustsson <git.fexilus@gmail.com>
2021-04-08 09:38:18 +0200

    Cleaned up script

--- scripts/lotka-volterra/find_a_independent.py
+++ scripts/lotka-volterra/find_a_independent.py
@@ -30,13 +30,18 @@ xi = Function("xi")(t, u2)
 #print(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[1].expand().collect(a, evaluate=False)[a])
 eta2 = Function("eta^2")(t, u2)
 
-eq_2_1_dict = get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[1].expand().collect(a, evaluate=False)[1].collect(eta1, evaluate=False)
+sym_conds = get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)),
+                                  jet_space, derivative_hints=(u1t, u2t))
+eq_2_1_dict = sym_conds[1].expand().collect(a, evaluate=False)[1].collect(eta1, evaluate=False)
 eta1 = - eq_2_1_dict[1] / eq_2_1_dict[eta1]
 
-function_monoids = list(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[0].expand().atoms(AppliedUndef))
-function_monoids += list(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[0].expand().atoms(Derivative))
+# The second symmetry condition is \equiv 0, so only first is dealt with
+sym_cond = get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)),
+                                 jet_space, derivative_hints=(u1t, u2t))[0]
+function_monoids = list(sym_cond.expand().atoms(AppliedUndef))
+function_monoids += list(sym_cond.expand().atoms(Derivative))
 
-a_u1_separated_equations = poly(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[0].expand(), (a, u1)).coeffs()
+a_u1_separated_equations = poly(sym_cond.expand(), (a, u1)).coeffs()
 
 #print(dict(zip(function_monoids, list(linsolve(a_u1_separated_equations, function_monoids))[0])))
 c1 = symbols("c_1", real=True)
@@ -44,18 +49,22 @@ old_xi = xi
 xi = c1
 eta1 = eta1.replace(old_xi, xi).doit()
 
-eta2_solution = pdsolve(poly(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[0].expand(), (a, u1)).coeffs()[0])
+sym_cond = get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)),
+                                 jet_space, derivative_hints=(u1t, u2t))[0]
+eta2_solution = pdsolve(poly(sym_cond, (a, u1)).coeffs()[0])
 
 old_eta2 = eta2
 eta2 = eta2_solution.rhs
 eta1 = eta1.replace(old_eta2, eta2).doit()
 
-#print(poly(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[0].expand(), (a, u1)).coeffs()[1])
-
-arbitrary_func_name = list(poly(get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)), jet_space, derivative_hints=(u1t, u2t))[0].expand(), (a, u1)).coeffs()[1].atoms(AppliedUndef))[0].name
+sym_cond = get_lin_symmetry_cond(diff_eqs, Generator(xi, (eta1, eta2)),
+                                 jet_space, derivative_hints=(u1t, u2t))[0]
+decomp_sym_cond_1 = poly(sym_cond, (a, u1)).coeffs()[1]
+#print(decomp_sym_cond_1)
+arbitrary_func_name = list(decomp_sym_cond_1.atoms(AppliedUndef))[0].name
 
 eta2 = eta2.replace(Function(arbitrary_func_name)(Wild("x")), 0)
 #eta1 = eta1.replace(Function(arbitrary_func_name)(Wild("x")), 0).doit()
-eta1 = 0 # Temporary fix because of bug fixed after release of sympy 1.7.1
+eta1 = 0 # Temporary fix because of bug only fixed after release of sympy 1.7.1
 
 print(Generator(xi, (eta1, eta2)))
