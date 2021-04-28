814e4648dced7de6505150f62c8eab8a0640cff1
Felix Augustsson <git.fexilus@gmail.com>
2021-04-28 18:05:33 +0200

    Updated symmetries and notation

---
+++ scripts/gompertz/confirm_autonomous_symmetries.py
@@ -0,0 +1,75 @@
+from sympy import symbols, ln, exp, Piecewise, solve, Equality, expand_log
+
+from symmetries.jetspace import JetSpace
+from symmetries.generator import generator_on
+from symmetries.symcond import get_lin_symmetry_cond
+
+# Time
+t = time = symbols("t", real=True)
+# States
+W = state = symbols("W", positive=True)
+
+# Jet space and derivative coordinate
+jet_space = JetSpace(time, state, 1)
+
+Wt = jet_space.fibres[W][(1,)]
+
+# Parameters
+kG, A = symbols("k_G A", positive=True)
+
+# Differential equations
+autonomous_equation = Wt + kG * W * ln(W / A)
+
+Generator = generator_on(jet_space.original_total_space)
+# Generators
+X_aut1 = Generator(exp(kG * t) * ln(W / A), 0)
+X_aut2 = Generator(0, exp(-kG * t) * W)
+X_aut3 = Generator(0, W * ln(W / A))
+X_aut4 = Generator(1, 0)
+X_aut5 = Generator(t, W * ln(W / A) * ln(abs(ln(W / A))))
+X_aut6 = Generator(exp(-kG * t), -kG * exp(-kG * t) * W * ln(W))
+
+generators = [X_aut1, X_aut2, X_aut3, X_aut4, X_aut5, X_aut6]
+
+# Confirm generators for respective equations
+all_autonomous_confirmed = True
+for generator in generators:
+    sym_cond = get_lin_symmetry_cond(autonomous_equation, generator, jet_space,
+                                     derivative_hints=Wt)
+    if not sym_cond.expand().together().is_zero:
+        # Test if the symmetry condition holds piecewise
+        if isinstance(sym_cond.expand().together().simplify(), Piecewise):
+
+            piecewise_holds = True
+            for expr, cond in sym_cond.expand().together().simplify().args:
+
+                if isinstance(cond, Equality):
+
+                    # If the condition can never happen, skip checking expr
+                    if not solve(cond):
+                        continue
+
+                    expanded_expr = expand_log(expr)
+                    expanded_lhs = expand_log(cond.lhs)
+                    cond_expr = expanded_expr.subs(expanded_lhs, cond.rhs)
+
+                else:
+                    cond_expr = expr
+
+                if not cond_expr.expand().together().is_zero:
+                    piecewise_holds = False
+
+            if piecewise_holds:
+
+                print(f"The generator {generator} is piecewise a "
+                      "symmetry of the autonomous Gompertz model")
+                continue
+
+        all_autonomous_confirmed = False
+
+        print(f"The generator {generator} is not a symmetry of the autonomous "
+              "Gompertz model")
+
+if all_autonomous_confirmed:
+    print("All expected generators are generators of the autonomous Gompertz "
+          "model")
---
+++ scripts/gompertz/confirm_classical_symmetries.py
@@ -0,0 +1,45 @@
+from sympy import symbols, ln, exp
+
+from symmetries.jetspace import JetSpace
+from symmetries.generator import generator_on
+from symmetries.symcond import get_lin_symmetry_cond
+
+# Time
+t = time = symbols("t", real=True)
+# States
+W = state = symbols("W", nonnegative=True)
+
+# Jet space and derivative coordinate
+jet_space = JetSpace(time, state, 1)
+
+Wt = jet_space.fibres[W][(1,)]
+
+# Parameters
+kG, Ti = symbols("k_G T_i")
+
+# Differential equations
+classical_equation = Wt - kG * exp(-kG * (t - Ti)) * W
+
+Generator = generator_on(jet_space.original_total_space)
+# Generators
+X_cla1 = Generator(exp(kG * t), 0)
+X_cla2 = Generator(W*exp(kG * t + exp(-kG * (t - Ti))), 0)
+X_cla3 = Generator(0, exp(-exp(Ti * kG) * exp(-kG * t)))
+X_cla4 = Generator(0, W)
+X_cla5 = Generator(1, - kG * W * ln(W))
+
+generators = [X_cla1, X_cla2, X_cla3, X_cla4, X_cla5]
+
+all_classical_confirmed = True
+for generator in generators:
+    sym_cond = get_lin_symmetry_cond(classical_equation, generator, jet_space,
+                                     derivative_hints=Wt)
+    if not sym_cond.expand().together().is_zero:
+        all_classical_confirmed = False
+
+        print(f"The generator {generator} is not a symmetry of the classical "
+              "Gompertz model")
+
+if all_classical_confirmed:
+    print("All expected generators are generators of the classical Gompertz "
+          "model")
