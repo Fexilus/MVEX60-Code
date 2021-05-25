60e5e0c5b1dbec38a2786106abae2174f51a8bb7
Felix Augustsson <git.fexilus@gmail.com>
2021-05-25 16:33:22 +0200

    Change triangle rotation to exterior arrows + text

--- scripts/generic/plot_triangle_symmetries.py
+++ scripts/generic/plot_triangle_symmetries.py
@@ -4,12 +4,14 @@ import os.path
 import numpy as np
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Arc
-
-from symmetries.visualize.arrowpath import WithArrowStroke
+from matplotlib.patches import FancyArrowPatch
 
 
 def plot(save_path=None, file_name="triangles.eps"):
+    plt.rc("text", usetex=True)
+    plt.rc("font", family="serif")
+    plt.rc("mathtext", fontset="cm")
+
     triangle1_vertices = np.array([[0, 0], [0.5, np.sqrt(0.75)], [1, 0]])
     triangle2_vertices = triangle1_vertices + np.array([1.2, 0])
     triangle3_vertices = triangle2_vertices + np.array([1.2, 0])
@@ -41,24 +43,33 @@ def plot(save_path=None, file_name="triangles.eps"):
     ax.scatter(points3[:, 0], points3[:, 1], marker="X", s=60, color="C2",
                zorder=3)
 
-    arc2 = Arc(triangle2_vertices.sum(axis=0) / 3,
-               width=np.sqrt(0.75) / 2,
-               height=np.sqrt(0.75) / 2,
-               theta1=5 * 30,
-               theta2=9 * 30,
-               path_effects=[WithArrowStroke(spacing=70.1, scaling=7)])
-    arc3 = Arc(triangle3_vertices.sum(axis=0) / 3,
-               width=np.sqrt(0.75) / 2,
-               height=np.sqrt(0.75) / 2,
-               theta1=5 * 30,
-               theta2=13 * 30,
-               path_effects=[WithArrowStroke(spacing=150.2, scaling=7)])
+    arc2 = FancyArrowPatch(triangle2_vertices[1], triangle2_vertices[0],
+                           arrowstyle="->", mutation_scale=10,
+                           connectionstyle="Angle3, angleA=180, angleB=120",
+                           shrinkA=10, shrinkB=10)
+    arc3 = FancyArrowPatch(triangle3_vertices[1], triangle3_vertices[2],
+                           arrowstyle="->", mutation_scale=10,
+                           connectionstyle="Angle3, angleA=180, angleB=240",
+                           shrinkA=10, shrinkB=10)
 
     ax.add_patch(arc2)
     ax.add_patch(arc3)
 
+    ax.text(*(triangle1_vertices[1] + np.array((0, 0.1))),
+            "No rotation",
+            horizontalalignment="center")
+    ax.text(*(triangle2_vertices[1] + np.array((0, 0.1))),
+            r"Rotation by $120^{\circ}$",
+            horizontalalignment="center")
+    ax.text(*(triangle3_vertices[1] + np.array((0, 0.1))),
+            r"Rotation by $-120^{\circ}$",
+            horizontalalignment="center")
+
     ax.axis("equal")
+    ax.set(xlim=(0, 3.4), ylim=(0, np.sqrt(0.75)))
     ax.axis("off")
+
+    fig.tight_layout()
 
     if save_path:
         file_path = os.path.join(save_path, file_name)
