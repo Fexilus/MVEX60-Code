5e2e534cf63f7b6969f8911bd35f5c4812428583
Felix Augustsson <git.fexilus@gmail.com>
2021-05-25 16:31:13 +0200

    Added a bounding box to crop jet surface figure

--- scripts/generic/plot_jet_surface.py
+++ scripts/generic/plot_jet_surface.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import to_rgba
 
 
+FIG_WIDTH = 9
+FIG_HEIGHT = 6
+
 def plot(save_path=None, file_name="jet-surface.pdf"):
     plt.rc("mathtext", fontset="cm")
 
@@ -19,13 +22,14 @@ def plot(save_path=None, file_name="jet-surface.pdf"):
 
     #xs, ys = np.meshgrid(x_vec, y_vec)
     # Matplotlib can't render transparent surfaces correctly since it
-    # lacks a 3d-rendering backend. This workaround works for a flat 
+    # lacks a 3d-rendering backend. This workaround works for a flat
     # surface
     xs, ys = np.meshgrid(xlim, ylim)
 
     zs = ys
 
-    fig, ax = plt.subplots(subplot_kw=dict(projection='3d'), figsize=(9, 6))
+    fig, ax = plt.subplots(subplot_kw=dict(projection="3d"),
+                           figsize=(FIG_WIDTH, FIG_HEIGHT))
 
     ax.plot_surface(xs, ys, zs, antialiased=False, color=to_rgba("C0", 0.85),
                     edgecolors=to_rgba("C0", 0), linewidths=0.0)
@@ -55,12 +59,22 @@ def plot(save_path=None, file_name="jet-surface.pdf"):
     ax.view_init(elev=12, azim=-68)
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
-    fig.tight_layout()
-
     if save_path:
         file_path = os.path.join(save_path, file_name)
-        plt.savefig(file_path, format="pdf",
-                    bbox_inches="tight")
+
+        # Create a bounding box of the axis that eliminates the vertical
+        # white space.
+        bbox = ax.get_position()
+        bbox.x1 += 0.04
+        bbox.y0 = 0.10
+        bbox.y1 = 0.85
+        # Convert the bounding box to inches
+        bbox.x0 *= FIG_WIDTH
+        bbox.x1 *= FIG_WIDTH
+        bbox.y0 *= FIG_HEIGHT
+        bbox.y1 *= FIG_HEIGHT
+
+        plt.savefig(file_path, format="pdf", bbox_inches=bbox)
 
 
 if __name__ == "__main__":
